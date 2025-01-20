01ecd62af1b11a213396fd995b37a26907358409
Jose-Prince <joseprince148@gmail.com>
2025-01-20 10:47:13 -0600

    Feat: Lazy.nvim configuration

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- Initial configuration with Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then 
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+require("settings")
+require("keymaps")
